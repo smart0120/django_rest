15cd8116d801565140f2813a5dfcf2cdcd7c6631
smart0120 <gadrgon0305@gmail.com>
2021-04-26 11:00:33 -0300

    fix: issue in get_colors function

--- restapi/views.py
+++ restapi/views.py
@@ -9,28 +9,29 @@ def get_colors(request):
     if(request.method=='POST'):
         colors = []
         for i in range(5):
-            # Generate random rgb color
-            rgb_color = rgb_random_color()
-
-            # Convert RGB to HSL if it's true
+            # Gernrate RGB if it's True
             if random.choice((True, False)):
-                hsl_color = colorsys.rgb_to_hls(rgb_color[0], rgb_color[1], rgb_color[2])
+                rgb_color = rgb_random_color()
                 colors.append({
-                    "type": 'hsl',
-                    "red": hsl_color[0],
-                    "green": hsl_color[1],
-                    "blue": hsl_color[2]
+                    "type": 'rgb',
+                    "red": rgb_color[0],
+                    "green": rgb_color[1],
+                    "blue": rgb_color[2]
                 })
             else:
+                hsl_color = hsl_random_color()
                 colors.append({
-                    "type": "rgb",
-                    "hue": rgb_color[0],
-                    "saturation": rgb_color[1],
-                    "lightness": rgb_color[2]
+                    "type": "hsl",
+                    "hue": hsl_color[0],
+                    "saturation": hsl_color[1],
+                    "lightness": hsl_color[2]
                 })
-            colors.append(rgb_random_color())
         return JsonResponse(colors, safe=False)
 
 def rgb_random_color():
     color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    return color
+
+def hsl_random_color():
+    color = [random.randint(0, 360), random.randint(0, 100), random.randint(0, 100)]
     return color
